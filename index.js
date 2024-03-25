bc699022dcb7458d56ace4a6de1861782efa9f21
amandasuzzanne <amandasuzanne12@gmail.com>
2024-03-25 11:47:20 +0300

    Complete arithmetic lab exercise

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-
 // Write your code here
+// multiply - is an equation whose multiplied results will equal 62
+let multiply;
+let num1 = 31;
+let num2 = 2;
+
+multiply = num1 * num2;
+
+
+// random - generates a random integer greater than 0
+let random;
+random = Math.floor(Math.random() * 10) + 1;
+
+
+// mod - is an equation that calculates a remainder that is equal to 4
+let mod;
+let num3 = 16;
+let num4 = 6;
+
+mod = num3 % num4;
+
+
+//max - will return 20 as the highest number in the set
+let max;
+
+max = Math.max(2, 4, 6, 8, 10, 12, 13, 14, 16, 18, 20);
